947ef160796f0da5f8a7791710035f9718b420cc
HennsenA <3030119040108@ingenieria.usac.edu.gt>
2026-09-18 23:10:58 -0600

    Estructuracion inicial de la clase

---
+++ Controller/ManejoXml.cs
@@ -0,0 +1,26 @@
+﻿using IPC2_Proyecto2_S22026_202500286.Models;
+
+namespace IPC2_Proyecto2_S22026_202500286.Controller
+{
+    public class ManejoXml
+    {
+        private string Ruta;
+        public ManejoXml(string ruta)
+        {
+            Ruta = ruta;
+        }
+
+        public void CargaArchivo()
+        {
+
+        }
+        public ArbolCategoria CargarCategorias()
+        {
+
+        }
+        public ArbolAvl CargarLibros()
+        {
+
+        }
+    }
+}
