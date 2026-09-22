121b6d59809b3ff34dd6d270875edd7f645f5bb6
HennsenA <3030119040108@ingenieria.usac.edu.gt>
2026-09-22 16:31:12 -0600

    Implementacion de paginas de busqueda de libro

---
+++ Pages/BuscarLibro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IPC2_Proyecto2_S22026_202500286.Pages
+{
+    public class BuscarLibroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
