d0687ce2fd7cb952768a0990b1c35b808ff66de1
HennsenA <3030119040108@ingenieria.usac.edu.gt>
2026-09-22 14:50:11 -0600

    Implementacion de paginas para registro y eliminacion de elementos

---
+++ Pages/EliminarLibro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IPC2_Proyecto2_S22026_202500286.Pages
+{
+    public class EliminarLibroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
